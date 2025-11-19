237e110acb6f4c56a2d92444132d0d490ff2158d
Bhanu <bhanu99517@gmail.com>
2025-11-19 21:41:45 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,21 @@
+import { MongoClient, MongoClientOptions } from 'mongodb';
+import { attachDatabasePool } from '@vercel/functions';
+
+const options: MongoClientOptions = {
+  appName: "devrel.vercel.integration",
+  maxIdleTimeMS: 5000
+};
+const client = new MongoClient(process.env.MONGODB_URI, options);
+   
+// Attach the client to ensure proper cleanup on function suspension
+attachDatabasePool(client);
+
+// Export a module-scoped MongoClient to ensure the client can be shared across functions.
+export default client; 
+
+
+
+
 // backend/database.js
 const mongoose = require("mongoose");
 
