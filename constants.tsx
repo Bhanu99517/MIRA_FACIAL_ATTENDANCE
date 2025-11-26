2df3e661d24b1052ec818966c2a11eda0d5e7681
Bhanu <bhanu99517@gmail.com>
2025-11-26 21:54:31 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -324,6 +324,18 @@ export const Icons = {
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 3a2.25 2.25 0 00-2.25 2.25h4.5A2.25 2.25 0 0012 3z" opacity="0.5" />
     </svg>
   ),
+  // Plus icon for adding new items
+  plusIcon: (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
+  ),
+  // Arrow left icon for back navigation
+  arrowLeftIcon: (props: React.SVGProps<SVGSVGElement>) => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+    </svg>
+  ),
 };
 
 // Defines the structure of the navigation links for the sidebar.
